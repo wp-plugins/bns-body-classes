2e88c06a4f863e63a8e56b057e5c76d91ffbea80
cais <cais@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-09 15:54:06 +0000

    * Added $classes parameter in calendars to be able to append new date classes
    git-svn-id: https://plugins.svn.wordpress.org/bns-body-classes/tags/0.5.2@694635 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bns-body-classes.php
@@ -0,0 +1,261 @@
+<?php
+/*
+Plugin Name: BNS Body Classes
+Plugin URI: http://buynowshop.com/plugins/bns-body-classes/
+Description: Simple plugin that adds classes to the `body_class` output upon activation, including a full list of date classes.
+Version: 0.5.2
+Text Domain: bns-bc
+Author: Edward Caissie
+Author URI: http://edwardcaissie.com/
+License: GNU General Public License v2
+License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+/**
+ * BNS Body Classes
+ *
+ * Simple plugin that adds classes to the `body_class` output upon activation,
+ * including: a full list of date related classes; a few holiday classes; and,
+ * a generic sample full calendar extension.
+ *
+ * @package     BNS_Body_Classes
+ * @version     0.5.2
+ *
+ * @link        http://buynowshop.com/plugins/bns-body-classes/
+ * @link        https://github.com/Cais/bns-body-classes/
+ * @link        http://wordpress.org/extend/plugins/bns-body-classes
+ *
+ * @author      Edward Caissie <edward.caissie@gmail.com>
+ * @copyright   Copyright (c) 2012-2013, Edward Caissie
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.
+ *
+ * You may NOT assume that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ *
+ *      Free Software Foundation, Inc.
+ *      51 Franklin St, Fifth Floor
+ *      Boston, MA  02110-1301  USA
+ *
+ * The license for this software can also likely be found here:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Last revised January 29, 2012
+ * @internal    Project To-Do List
+ * @todo add options to specify when custom class is applied (i.e.: which template types it applies to)
+ *
+ * @version 0.3
+ * @date    December 1, 2012
+ *
+ * @version 0.4
+ * @date    December 11, 2012
+ * Add generic sample calendar extension
+ *
+ * @version 0.5
+ * @date    April 1, 2013
+ * Code formatting and code block termination comments
+ *
+ * @version     0.5.2
+ * @date        April 9, 2013
+ * Added $classes parameter in calendars to be able to append new date classes
+ */
+
+class BNS_Body_Classes {
+    /**
+     * Constructor
+     *
+     * @package BNS_Body_Classes
+     *
+     * @uses    add_filter
+     */
+    function __construct() {
+        /** Add Body Classes */
+        add_filter( 'body_class', array( $this, 'bns_body_classes' ) );
+    } /** End function - constructor */
+
+
+    /**
+     * BNS Body Classes
+     *
+     * Add additional classes to the core `body_class` function
+     *
+     * @package BNS_Body_Classes
+     * @since   0.1
+     *
+     * @param   $classes
+     *
+     * @return  array - $classes, an array of classes to be added to `body_class`
+     */
+    function bns_body_classes( $classes ) {
+        /** Add self reference to default body classes; also serves as very simple example */
+        $classes[] = 'plugin-bns-body-classes';
+
+        /** Add child-theme-<Name> to default body classes */
+        if ( is_child_theme() ) {
+            $classes[] = 'child-theme-' . sanitize_html_class( get_option( 'stylesheet' ) );
+        } /** End if - is child theme */
+
+        /** Add theme-<Name> to default body classes */
+        $classes[] = 'theme-' . sanitize_html_class( get_option( 'template' ) );
+
+        /** Add Current Dates classes */
+        $classes[] = $this->current_dates();
+
+        /** Add Holiday Classes */
+        $classes[] = $this->holidays();
+
+        /** @var $more_classes - for additional classes from extensions */
+        $more_classes = apply_filters( 'bnsbc_more_classes', '' );
+        $classes[] .= $more_classes;
+
+        /** @var $classes - overwrite the body classes at the last minute class additions? */
+        $classes = apply_filters( 'bns_body_classes', $classes );
+
+        return $classes;
+
+    } /** End function - bns body classes */
+
+
+    /**
+     * Current Dates
+     * Add classes representing the current date
+     *
+     * @package BNS_Body_Classes
+     * @since   0.3
+     *
+     * @return string
+     */
+    function current_dates() {
+        /** Current Date Classes */
+        $date_classes = '';
+
+        /** Year */
+        $current_year = date( 'Y' );
+        $date_classes .= 'year-' . $current_year;
+        $leap_year = date( 'L' );
+        if ( '1' == $leap_year ) {
+            $date_classes .= ' leap-year';
+        } /** End if - leap year */
+        /** Month */
+        $current_month_numeric = date( 'm' );
+        $date_classes .= ' month-' . $current_month_numeric;
+        $current_month_short = date( 'M' );
+        $date_classes .= ' month-' . strtolower( $current_month_short );
+        $current_month_long = date( 'F' );
+        $date_classes .= ' month-' . strtolower( $current_month_long );
+        /** Day */
+        $current_day_of_month = date( 'd' );
+        $date_classes .= ' day-' . $current_day_of_month;
+        $current_day_of_week_short = date( 'D' );
+        $date_classes .= ' day-' . strtolower( $current_day_of_week_short );
+        $current_day_of_week_long = date( 'l' );
+        $date_classes .= ' day-' . strtolower( $current_day_of_week_long );
+        /** Time: Hour */
+        $current_24_hour = date( 'H' );
+        $date_classes .= ' hour-' . $current_24_hour;
+        $current_12_hour = date( 'ha' );
+        $date_classes .= ' hour-' . $current_12_hour;
+
+        return $date_classes;
+
+    } /** End function - current dates */
+
+
+    /**
+     * Holidays
+     * Common Holidays and Observances related classes
+     *
+     * @package BNS_Body_Classes
+     * @since   0.3
+     *
+     * @return string
+     *
+     * @version 0.4
+     * @date    December 13, 2012
+     * Added more common dates: Valentine's Day, St. Patrick's Day, April Fool's
+     * Day, Halloween, Remembrance Day
+     */
+    function holidays() {
+        /** @var $holidays - initialize as no holidays */
+        $holidays = '';
+
+        /** New Year's Day */
+        if ( ( '01' == date( 'm' ) ) && ( '01' == date( 'd' ) ) && ( 18 < date( 'H' ) ) ) {
+            $holidays .= ' new-years-eve';
+        } /** End if - new years eve */
+        if ( ( '01' == date( 'm' ) ) && ( '01' == date( 'd' ) ) ) {
+            $holidays .= ' new-years new-years-day';
+        } /** End if - new years day */
+
+        /** Valentine's Day */
+        if ( ( '02' == date( 'm' ) ) && ( '14' == date( 'd' ) ) ) {
+            $holidays .= ' valentines-day saint-valentines-day';
+        } /** End if - valentines day */
+
+        /** St. Patrick's Day */
+        if ( ( '03' == date( 'm' ) ) && ( '17' == date( 'd' ) ) ) {
+            $holidays .= ' st-patricks-day';
+        } /** End if - st patricks day */
+
+        /** April Fool's Day */
+        if ( ( '04' == date( 'm' ) ) && ( '01' == date( 'd' ) ) ) {
+            $holidays .= ' april-fools-day';
+        } /** End if - april fools day */
+
+        /** Halloween */
+        if ( ( '10' == date( 'm' ) ) && ( '31' == date( 'd' ) ) ) {
+            $holidays .= ' halloween';
+        } /** End if - halloween */
+
+        /** Remembrance Day */
+        if ( ( '11' == date( 'm' ) ) && ( '11' == date( 'd' ) ) ) {
+            $holidays .= ' remembrance-day poppy-day armistice-day veterans-day';
+        } /** End if - remembrance day */
+
+        /** Christmas */
+        if ( ( '12' == date( 'm' ) ) && ( '24' == date( 'd' ) ) && ( 18 < ( date( 'H' ) ) ) ) {
+            $holidays .= ' christmas-eve';
+        } /** End if - Christmas eve */
+        if ( ( '12' == date( 'm' ) ) && ( '25' == date( 'd' ) ) ) {
+            $holidays .= ' christmas christmas-day';
+        } /** End if - Christmas day */
+
+        return $holidays;
+
+    } /** End function - holidays */
+
+} /** End class - bns body classes */
+
+
+/** @var $bns_body_classes - new class instance */
+$bns_body_classes = new BNS_Body_Classes();
+
+
+/** Add BNSBC Options */
+include( 'bnsbc-options.php' );
+
+
+/** Add BNSBC Calendar if it is available */
+if ( is_readable( plugin_dir_path( __FILE__ ) . 'bnsbc-calendar.php' ) ) {
+    include( 'bnsbc-calendar.php' );
+} /** end if - is readable */
+
+/** Common 2013 Calendars */
+if ( '2013' == date( 'Y' ) ) {
+    include( 'bnsbc-2013-christian-calendar.php' );
+    include( 'bnsbc-2013-jewish-calendar.php' );
+} /** End if - year is 2013 */
+
+/** Common 2014 Calendars */
+if ( '2014' == date( 'Y' ) ) {
+    include( 'bnsbc-2014-christian-calendar.php' );
+    include( 'bnsbc-2014-jewish-calendar.php' );
+} /** End if - year is 2014 */
---
+++ readme.txt
@@ -0,0 +1,135 @@
+=== BNS Body Classes ===
+Contributors: cais
+Donate Link: http://buynowshop.com/
+Tags: CSS, body, plugin-only, holidays, seasonal, observances, calendar
+Requires at least: 3.0
+Tested Up To: 3.6
+Stable Tag: 0.5.2
+License: GNU General Public License v2
+License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+Simple plugin that adds classes to the `body_class` output upon activation, including a range of date related classes.
+
+== Description ==
+Simple plugin that adds classes to the `body_class` output upon activation, including: a full list of date related classes; a few holiday classes; and, a generic sample full calendar extension.
+
+== Installation ==
+This section describes how to install the plugin and get it working.
+
+1. Go to the "Plugins" menu in the Administration Panels ("dashboard").
+2. Click the 'Add New' link.
+3. Click the "Upload" link.
+4. Browse for the bns-body-classes.zip file on your computer; upload; and, install accordingly.
+5. Activate.
+
+-- or -
+
+1. Go to the "Plugins" menu in the Administration Panels ("dashboard").
+2. Click the 'Add New' link.
+3. Search for BNS Body Classes.
+4. Install.
+5. Activate.
+
+Please read this article for further assistance: http://wpfirstaid.com/2009/12/plugin-installation/
+
+== Frequently Asked Questions ==
+= What are the "Current Date" classes?
+The "Current Date" classes are a collection of classes dynamically created classes relevant to the date the reader lands
+on the site. These classes would include on December 1, 2012 at 5:00pm (GMT-5) the following:
+
+* year-2012
+* leap-year
+* month-12
+* month-dec
+* month-december
+* day-01
+* day-sat
+* day-saturday
+* hour-22
+* hour-10pm
+
+= How do I activate the calendar extension? =
+Included with the package is the file named 'bnsbc-calendar-sample.php'; simply rename this file to 'bnsbc-calendar.php', then edit it to add your own date-specific classes.
+
+As the specifically named file 'bnsbc-calendar.php' is not included with the standard plugin package it will survive any automatic updates via the WordPress Administration Panels.
+
+= How do I add a new class in the calendar extension? =
+After changing the sample file to 'bnsbc-calendar.php' simple find the date you want to add the class to and amend that line.
+
+For example, to add a class for the Ides of March you would edit the March 15 entry and add an appropriate class such as:
+
+* near line 138: if ( '15' == date( 'd' ) ) { $dates .= ''; } would change to if ( '15' == date( 'd' ) ) { $dates .= ' ides-of-march'; }
+
+NB: Remember to use "safe" characters for class names; and, add a single preceding space to eliminate the risk of classes running together.
+
+== Screenshots ==
+No screenshots are available.
+
+== Other Notes ==
+
+= Copyright 2012-2013  Edward Caissie  (email : edward.caissie@gmail.com) =
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2,
+  as published by the Free Software Foundation.
+
+  You may NOT assume that you can use any other version of the GPL.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  The license for this software can also likely be found here:
+  http://www.gnu.org/licenses/gpl-2.0.html
+
+== Upgrade Notice ==
+Please stay current with your WordPress installation, your active theme, and your plugins.
+
+== Changelog ==
+= 0.5.2 =
+* Released April 2013
+* Added $classes parameter in calendars to be able to append new date classes
+
+= 0.5.1 =
+* Released April 2013
+* Fixed wrong function being called by action hook
+
+= 0.5 =
+* Released April 2013
+* Added additional related tags to header block
+* Added Christian Calendars for the years 2013 and 2014
+* Added Jewish Calendars for the years 2013 and 2014
+* Code formatting and code block termination comments
+
+= 0.4 =
+* Released December 2012
+* Added conditional check on `option_classes`
+* Added generic (sample) calendar extension
+* Added more common dates: Valentine's Day, St. Patrick's Day, April Fool's Day, Halloween, Remembrance Day
+* i18n updates
+* Prepared for extension plugins
+* Refactored options to more appropriate naming conventions
+* Updated descriptions and FAQ to reflect new features
+
+= 0.3 =
+* Released December 2012
+* Add "Current Date" classes
+* Add "Holidays" classes (Christmas and New Years)
+* Refactored main plugin file to OOP code format
+
+= 0.2.2 =
+* Tested compatible with WordPress 3.4
+
+= 0.2.1 =
+* Corrected issue with initial empty array being fed to the `implode` function
+
+= 0.2 =
+* Add options page to include custom classes
+
+= 0.1 =
+* Initial Release - January 2012
